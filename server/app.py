53cb9c1a444a01acc8b38f43fb9f941e4e94ab55
Erica Abrahms <erica.abrahms@instrument.com>
2022-11-17 21:08:35 -0800

    add a temp static json file and return its contents to /score route

--- server/app.py
+++ server/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, Response
+with open('static/test_json.json') as f:
+  json_file = "".join(f.readlines())
 
 app = Flask(__name__)
+
+# url_for('static', filename='style.css')
 
 @app.route("/")
 def hello_world():
   return "<p>Hello world</p>"
+
+@app.post("/score")
+def score():
+  return Response(json_file, mimetype='application/json')
+  # 1. Serve static files
+  # 2. Fake JSON payload
+  # 3. ...make a website
+  # 4. make a database/s3 to store the uploaded files
+  # 5. deploy
+  # 6. Profit
