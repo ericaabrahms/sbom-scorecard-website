60745a7df18da0c73bf863b922c36ddb81a210e9
Erica Abrahms <erica.abrahms@instrument.com>
2022-11-18 09:06:57 -0800

    render from template, install tailwind

--- server/app.py
+++ server/app.py
@@ -1,18 +1,17 @@
-from flask import Flask, request, Response
+from flask import Flask, render_template, request, Response
 with open('static/test_json.json') as f:
   json_file = "".join(f.readlines())
 
 app = Flask(__name__)
 
-# url_for('static', filename='style.css')
-
 @app.route("/")
 def hello_world():
-  return "<p>Hello world</p>"
+  return render_template('hello.html')
 
 @app.post("/score")
 def score():
   return Response(json_file, mimetype='application/json')
+
   # 1. Serve static files
   # 2. Fake JSON payload
   # 3. ...make a website
