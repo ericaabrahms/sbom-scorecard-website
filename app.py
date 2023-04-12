723d2a1c95a0d404c5c34c6cd7489a64d0a229a1
Justin Abrahms <justin@abrah.ms>
2023-04-12 10:41:40 -0700

    Handle invalid json

--- app.py
+++ app.py
@@ -56,63 +56,69 @@ def score():
         return render_template(
             'scorecard.html',
         ), status_code
+  raw_payload = b"".join(f.stream.readlines())
+  try:
+    if f.mimetype == 'text/xml':
+        dom = xml.dom.minidom.parseString(raw_payload)
+        # not the most space efficient, but it's at least normalized.
+        payload = dom.toprettyxml()
+        pretty_payload = dom.toprettyxml()
 
-  if f.mimetype == 'text/xml':
-      payload = b"".join(f.stream.readlines())
-      dom = xml.dom.minidom.parseString(payload)
-      # not the most space efficient, but it's at least normalized.
-      payload = dom.toprettyxml()
-      pretty_payload = dom.toprettyxml()
+    elif f.mimetype == 'application/json':
+        payload = normalize_json(raw_payload)
+        pretty_payload = json.dumps(json.loads(payload), indent=4)
+    else:
+        pretty_payload = payload = raw_payload
 
-  elif f.mimetype == 'application/json':
-      payload = normalize_json(b"".join(f.stream.readlines()))
-      pretty_payload = json.dumps(json.loads(payload), indent=4)
-  else:
-      pretty_payload = payload = b"".join(f.stream.readlines())
+    checksum = sha1(payload).hexdigest()
+    f.seek(0) # reset cursor back to beginning after reading it out
 
-  checksum = sha1(payload).hexdigest()
-  f.seek(0) # reset cursor back to beginning after reading it out
+    # TODO: Openfeature?
 
-  # TODO: Openfeature?
-
-  if os.getenv("SKIP_UPLOAD", "false") != "true":
-    log.info("pushing %s to spaces", checksum)
-    client.put_object(
-        Bucket=os.getenv('SPACES_BUCKET'),
-        Key=f"{checksum}.json",
-        ContentType=f.mimetype,
-        ChecksumAlgorithm='sha1',
-        ChecksumSHA1=checksum,
-        Body=payload,
-        ACL='private',
-        Metadata={}
-    )
-
-  with TemporaryDirectory(f.filename) as d:
-      outfile = f'{d}/{f.filename}'
-      f.save(outfile)
-      log.debug("running cmd")
-      output = run(
-          f"./sbom-scorecard score {outfile} --outputFormat json",
-          hide='out', # hide stdout
-          warn=True, # don't throw exceptions on error
+    if os.getenv("SKIP_UPLOAD", "false") != "true":
+      log.info("pushing %s to spaces", checksum)
+      client.put_object(
+          Bucket=os.getenv('SPACES_BUCKET'),
+          Key=f"{checksum}.json",
+          ContentType=f.mimetype,
+          ChecksumAlgorithm='sha1',
+          ChecksumSHA1=checksum,
+          Body=payload,
+          ACL='private',
+          Metadata={}
       )
 
+    with TemporaryDirectory(f.filename) as d:
+        outfile = f'{d}/{f.filename}'
+        f.save(outfile)
+        log.debug("running cmd")
+        output = run(
+            f"./sbom-scorecard score {outfile} --outputFormat json",
+            hide='out', # hide stdout
+            warn=True, # don't throw exceptions on error
+        )
+
+    log.debug("outputting")
+    if output.ok:
+      print(output.stdout)
+      print(output.stderr)
+      score_data = json.loads(output.stdout)
+      status_code = 200
+    else:
+      log.warning("Error when running on file %s", checksum)
+      score_data = None
+      status_code = 400
+
+  except json.decoder.JSONDecodeError as e:
+    log.warning("Error parsing json %s", f.filename)
+    pretty_payload = raw_payload
+    score_data = None
+    status_code = 400
 
   # Note for Erica of the future:
   # use request.files to get to uploaded file
   # specifically, request.files('json-file')
 
-  log.debug("outputting")
-  if output.ok:
-    print(output.stdout)
-    print(output.stderr)
-    score_data = json.loads(output.stdout)
-    status_code = 200
-  else:
-    log.warning("Error when running on file %s", checksum)
-    score_data = None
-    status_code = 400
 
   return render_template(
     'scorecard.html',
