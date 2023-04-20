860dc006d0b03da48ced4cde490683603b8cf992
Justin Abrahms <justin@abrah.ms>
2023-04-20 14:14:22 -0700

    Support linking to results. Fixes #13

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json, os, boto3, botocore, re
 from hashlib import sha1
 from tempfile import TemporaryDirectory
 from invoke import run
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, redirect, url_for
 from werkzeug.utils import secure_filename
 import logging
 import xml.dom.minidom
@@ -37,63 +37,46 @@ def normalize_json(js: str):
         ensure_ascii=False
     ).encode('utf-8')
 
+def pretty_and_normalized(raw: str) -> (str, str):
+    try:
+        normalized = normalize_json(raw)
+        pretty_payload = json.dumps(json.loads(normalized), indent=4)
+    except json.decoder.JSONDecodeError:
+        dom = xml.dom.minidom.parseString(raw)
+        # not the most space efficient, but it's at least normalized.
+        normalized = dom.toprettyxml().encode('utf-8')
+        pretty_payload = dom.toprettyxml()
+    except:
+        import pdb; pdb.set_trace()
+        pretty_payload = normalized = raw
+
+    return pretty_payload, normalized
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def homepage():
   return render_template('hello.html')
 
 def add_spaces_to_name(name):
     return re.sub( r"([A-Z])", r" \1", name)
 
-@app.post("/score")
-def score():
-  f = request.files['json-file']
+@app.route("/score/<sha1checksum>")
+def get_score(sha1checksum):
+    with TemporaryDirectory(sha1checksum) as d:
+        filepath = os.path.join(d, sha1checksum)
+        if not os.path.exists(filepath):
+            with open(filepath, 'wb') as f:
+                client.download_fileobj(os.getenv('SPACES_BUCKET'), f"{sha1checksum}.json", f)
 
-  log.debug("saving")
-  if f.filename == '':
-        status_code = 400
-        return render_template(
-            'scorecard.html',
-        ), status_code
-  raw_payload = b"".join(f.stream.readlines()).decode('utf-8')
-  try:
-    if f.mimetype == 'text/xml':
-        dom = xml.dom.minidom.parseString(raw_payload)
-        # not the most space efficient, but it's at least normalized.
-        payload = dom.toprettyxml().encode('utf-8')
-        pretty_payload = dom.toprettyxml()
+        with open(filepath, 'r') as f:
+            contents = ''.join(f.readlines())
 
-    elif f.mimetype == 'application/json':
-        payload = normalize_json(raw_payload)
-        pretty_payload = json.dumps(json.loads(payload), indent=4)
-    else:
-        pretty_payload = payload = raw_payload
+        pretty_payload, normalized = pretty_and_normalized(contents)
 
-    checksum = sha1(payload).hexdigest()
-    f.seek(0) # reset cursor back to beginning after reading it out
-
-    # TODO: Openfeature?
-
-    if os.getenv("SKIP_UPLOAD", "false") != "true":
-      log.info("pushing %s to spaces", checksum)
-      client.put_object(
-          Bucket=os.getenv('SPACES_BUCKET'),
-          Key=f"{checksum}.json",
-          ContentType=f.mimetype,
-          ChecksumAlgorithm='sha1',
-          ChecksumSHA1=checksum,
-          Body=payload,
-          ACL='private',
-          Metadata={}
-      )
-
-    with TemporaryDirectory(f.filename) as d:
-        outfile = f'{d}/{f.filename}'
-        f.save(outfile)
         log.debug("running cmd")
         output = run(
-            f"./sbom-scorecard score {outfile} --outputFormat json",
+            f"./sbom-scorecard score {filepath} --outputFormat json",
             hide='out', # hide stdout
             warn=True, # don't throw exceptions on error
         )
@@ -105,24 +88,46 @@ def score():
       score_data = json.loads(output.stdout)
       status_code = 200
     else:
-      log.warning("Error when running on file %s", checksum)
+      log.warning("Error when running on file %s", sha1checksum)
       score_data = None
       status_code = 400
 
-  except json.decoder.JSONDecodeError as e:
-    log.warning("Error parsing json %s", f.filename)
-    pretty_payload = raw_payload
-    score_data = None
-    status_code = 400
-
-  # Note for Erica of the future:
-  # use request.files to get to uploaded file
-  # specifically, request.files('json-file')
+    return render_template(
+      'scorecard.html',
+      score_data=score_data,
+      add_spaces_to_name=add_spaces_to_name,
+      json_data=pretty_payload,
+    ), status_code
 
 
-  return render_template(
-    'scorecard.html',
-    score_data=score_data,
-    add_spaces_to_name=add_spaces_to_name,
-    json_data=pretty_payload,
-  ), status_code
+@app.post("/score")
+def score():
+  f = request.files['json-file']
+
+  log.debug("saving")
+  if f.filename == '':
+        return render_template(
+            'scorecard.html',
+        ), 400
+  raw_payload = b"".join(f.stream.readlines()).decode('utf-8')
+  pretty_payload, payload = pretty_and_normalized(raw_payload)
+  checksum = sha1(payload).hexdigest()
+  import pdb; pdb.set_trace()
+
+  f.seek(0) # reset cursor back to beginning after reading it out
+
+  # TODO: Openfeature?
+  if os.getenv("SKIP_UPLOAD", "false") != "true":
+    log.info("pushing %s to spaces", checksum)
+    client.put_object(
+        Bucket=os.getenv('SPACES_BUCKET'),
+        Key=f"{checksum}.json",
+        ContentType=f.mimetype,
+        ChecksumAlgorithm='sha1',
+        ChecksumSHA1=checksum,
+        Body=payload,
+        ACL='private',
+        Metadata={}
+    )
+
+  return redirect(url_for('get_score', sha1checksum=checksum))
